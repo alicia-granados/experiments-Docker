dfb594a6da7f6993da6f6363a025090aceb3b6f9
alicia-granados <aliciagranados35@gmail.com>
2023-07-05 15:33:44 -0600

    ✨ feat:Cron-Ticker -aplicacion simple

---
+++ Cron-Ticker/app.js
@@ -0,0 +1,10 @@
+const cron = require('node-cron');
+
+let times=0;
+
+cron.schedule('1-59/5 * * * * *', () => {
+    times++;
+    console.log('tick cada 5 segnudos ', times);
+});
+
+console.log('Inicio');
